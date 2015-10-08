39357a46c0d90b78c341b0709fbbc578b7de9946
Kirk Pettinga <kirk.pettinga@gmail.com>
2015-10-08 14:16:49 +0700

    Built primer from commit 2db6ddf on branch master

--- js/docs.js
+++ js/docs.js
@@ -15,13 +15,37 @@
 
 	// Side Nav accordian
 	// -----------------------------------
-	$('.expand-sub-menu').click(function(e){
+	$('.sub-menu-toggle').click(function(e){
+		e.preventDefault();
+
 		var $this = $(this),
 			$parent = $this.parents('.top-level-menu'),
 			$subNav = $this.siblings('.nav-sub-menu');
 
 		$subNav.slideToggle(300);
 		$parent.toggleClass('open');
+
+	});
+
+}( window.jQuery, window, document ));
+
+(function( $, window, document ) {
+
+	// Masthead
+	// -----------------------------------
+	var $window = $(window),
+		winTop = $window.scrollTop(),
+		$masthead = $('.masthead'),
+		threshold = $('.jumbotron').outerHeight() - $masthead.outerHeight();
+
+	$window.scroll(function(){
+		winTop = $window.scrollTop();
+		
+		if ( winTop >= threshold ) {
+			$masthead.addClass('show-title');
+		} else {
+			$masthead.removeClass('show-title');
+		}
 	});
 
 }( window.jQuery, window, document ));
