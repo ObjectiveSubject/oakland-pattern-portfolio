03972837ff2aac8849070f3c9f51a41323d60383
AndrewMowe <andrew@team-eight.com>
2015-10-13 15:36:30 -0500

    Built primer from commit b433a59 on branch master

--- js/oakland.js
+++ js/oakland.js
@@ -230,3 +230,16 @@
 	});
 
 }( window.jQuery, window, document ));
+
+(function( $, window, document ) {
+
+	// Home Services Controls
+	// -----------------------------------
+	var $bigMenuItem = $('.big-menu .menu-item');
+
+	$bigMenuItem.on('click', function(e) {
+		e.preventDefault();
+		$(this).toggleClass('selected').children('.big-menu-group').slideToggle('fast');
+	});
+
+}( window.jQuery, window, document ));
